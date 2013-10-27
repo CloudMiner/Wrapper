7024faaa2e81d300a75ea7dda121799e6ae8f048
nilksermot <tomresklin@hotmail.com>
2013-10-27 23:04:24 +0100

    parser inciado

--- Main/ServerParser.py
+++ Main/ServerParser.py
@@ -17,4 +17,26 @@ class ServerParser(object):
         print "Parser created"
         
     def parse_cmd(self, cmd):
-        return cmd
+        l1 = cmd.split() #Aqui se puede anyadir el parametro correspondiente al separador (si es el espacio no se pone)
+        #for s in l1:
+        #    print s
+        if len(l1) < 1:
+            print "ERR: no commands issued..."
+        elif len(l1) == 1:
+            if l1[0].lower() == "stop":
+                print "stop mining!"
+            elif l1[0].lower() == "start":
+                print "ERR, crypto-currency parameter missing..."
+            else:
+                print "ERR, command not recognized: " + l1[0]
+        elif len(l1) == 2:
+            if l1[0].lower() == "start":
+                if l1[1].lower() == "btc":
+                    print "start mining for BitCoins!"
+                elif l1[1].lower() == "ltc":
+                    print "start mining for LiteCoins!"
+                else:
+                    print "ERR, crypto-currency not recognized: " + l1[1]
+            else:
+                print "ERR, command not recognized: " + l1[0]
+        return l1
--- Main/__init__.py
+++ Main/__init__.py
@@ -2,6 +2,7 @@ import logging
 import asyncore
 from EchoServer import EchoServer
 from EchoClient import EchoClient
+from ServerParser import ServerParser
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
@@ -15,5 +16,15 @@ if __name__ == '__main__':
     client = EchoClient(ip, port, message=open('instructions.txt', 'r').read())
     
     asyncore.loop()
+    '''p = ServerParser()
+    p.parse_cmd("")
+    p.parse_cmd("ss")
+    p.parse_cmd("start")
+    p.parse_cmd("stop")
+    p.parse_cmd("una prueba")
+    p.parse_cmd("start prueba")
+    p.parse_cmd("start ltc")
+    p.parse_cmd("sTArt lTc")
+    p.parse_cmd("sTArt bTc")'''
     
     
