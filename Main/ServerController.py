ff23c2cfa4404f68bfe8529056dee99a74ecedaa
nilksermot <tomresklin@hotmail.com>
2013-11-05 23:16:11 +0100

    mods en servercontroller

--- Main/ServerController.py
+++ Main/ServerController.py
@@ -19,16 +19,16 @@ class ServerController(object):
         print "Controller created"
     
     def monitor(self):
-        sleep(5)
         i = 0
         while True:        
             print str(i) +" MH/s"
             i += 1
+            sleep(5) # este sleep si debería hacer paramétrico, indica el tiempo de refresco, debería ser el mismo que para el miner
             
     def execute_cmd(self, cmd):
         print str(cmd) + " executed"
         p = Process(target = self.monitor)
         p.start()
-        sleep(6)
+        sleep(30) # este sleep es innecesario, se reemplazaría por la recepción de la orden "STOP"
         p.terminate()
         sleep(1)
