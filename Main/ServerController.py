85b9ae726bb6d93232d26fda30906f68236cecc5
Arturo Pareja <arturo.pareja.garcia@gmail.com>
2013-11-05 14:06:49 +0100

    Process start y terminate movidos a la clase ServerController. Ejecuciones de prueba.

--- Main/ServerController.py
+++ Main/ServerController.py
@@ -4,11 +4,13 @@ Created on 25/10/2013
 @author: hackturo
 '''
 
+from time import sleep
+from multiprocessing import Process
+
 class ServerController(object):
     '''
     classdocs
     '''
-
 
     def __init__(self):
         '''
@@ -16,5 +18,17 @@ class ServerController(object):
         '''
         print "Controller created"
     
+    def monitor(self):
+        sleep(5)
+        i = 0
+        while True:        
+            print str(i) +" MH/s"
+            i += 1
+            
     def execute_cmd(self, cmd):
-        print cmd + " executed"
+        print str(cmd) + " executed"
+        p = Process(target = self.monitor)
+        p.start()
+        sleep(6)
+        p.terminate()
+        sleep(1)
