2d9e40a162e622fa6bc57fe94718fb8ab2fa5772
nilksermot <nilksermot@gmail.com>
2013-11-19 20:39:21 +0100

    1ra demo funcionando

--- Main/HeadNode.py
+++ Main/HeadNode.py
@@ -8,7 +8,7 @@ import asyncore
 import subprocess
 from time import sleep
 import datetime
-#import time
+import time
 
 from time import sleep
 from TaskMaster import TaskMaster
@@ -29,9 +29,28 @@ if __name__ == '__main__':
     address_client = ('127.0.0.1', 5006)
     task_process(address_client, "start BTC")
     print "==============COMANDO 1 TERMINADO==================="
-    sleep(30)
+    sleep(60)
     task_process(address_client, "stop")
     print "==============COMANDO 2 TERMINADO===================" 
-    '''ts = time.time()
+    
+    '''
+    ts = time.time()
     time = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    print time'''
+    print time
+    
+    with open("log.txt",'r') as fh:
+        first = next(fh).decode()
+        l = len(first)
+        fh.seek(-100, 2)
+        last = fh.readlines()[-1].decode()
+        f = open('stats.txt','a')
+        if (last.find('avg:') > -1) :
+            mhs = last[last.find('avg:')+4 : last.find('u:')-1] + 'MH/s'
+            ts = time.time()
+            tim = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+            f.write(tim+" "+mhs+"\n")
+            print tim+" "+mhs
+        else:
+            f.write(last)
+            print last
+    '''
--- Main/WorkerController.py
+++ Main/WorkerController.py
@@ -11,6 +11,8 @@ import signal
 import subprocess
 import datetime
 import time
+import multiprocessing
+import array
 
 class WorkerController(object):
     '''
@@ -34,31 +36,62 @@ class WorkerController(object):
             i += 1
         return
     '''        
-    
-    def monitor(self):
+    def monitor_gpu(self):
+        f = open('stats.txt','a')
+        sleep(10)
         while True:
             with open("log.txt",'rb') as fh:
                 first = next(fh).decode()
-                fh.seek(-1024, 2)
+                fh.seek(-100, 2)
                 last = fh.readlines()[-1].decode()
-            f = open('stats.txt','a')
             if (last.find('avg:') > -1) :
                 mhs = last[last.find('avg:')+4 : last.find('u:')-1] + 'MH/s'
                 ts = time.time()
                 tim = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
                 f.write(tim+" "+mhs+"\n")
                 print tim+" "+mhs
-            f.close()
+            else:
+                print last
             sleep(5)
-    
+        f.close()
+            
+    def monitor_cpu(self):
+        cpus = multiprocessing.cpu_count()
+        last_line = ''
+        while True:
+            f_stats = open('stats.txt','a')
+            sleep(10)
+            with open("log.txt",'rb') as f_log:
+                first = next(f_log).decode()
+                f_log.seek(-100, 2)
+                lines = f_log.readlines()
+                if(last_line != lines[-1].decode()):
+                    last_line = lines[-1].decode()
+                    hashes = 0
+                    for i in range (1,cpus+1):
+                        line_cpu = lines[-i].decode()
+                        if (line_cpu.find('hashes') > -1) :
+                            hashes = hashes + int(line_cpu[line_cpu.find('hashes')+8:line_cpu.find('hashes')+12])
+                            hash_rate = line_cpu[line_cpu.find('hashes')+13:len(line_cpu)-1]
+                        else:
+                            hashes = 0
+                            hash_rate = '???'
+                    ts = time.time()
+                    tim = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+                    f_stats.write(tim+" "+str(hashes)+" "+hash_rate+"\n")
+                    print tim+" "+str(hashes)+" "+hash_rate
+            f_stats.close()
+        
     def execute_cmd(self, cmd):
         print str(cmd) + " to be executed"
         if str(cmd[0]) == "start":
-            self.p_monitor = Process(target = self.monitor)
-            self.p_monitor.start()
             # The os.setsid() is passed in the argument preexec_fn so
             # it's run after the fork() and before  exec() to run the shell.
-            self.p_miner = subprocess.Popen('bfgminer -o http://api2.bitcoin.cz:8332 -u tomresklin.worker1 -p nDBb37wH --real-quiet > log.txt', shell=True, preexec_fn=os.setsid)
+            #self.p_miner = subprocess.Popen('bfgminer -o http://api2.bitcoin.cz:8332 -u tomresklin.worker1 -p nDBb37wH --real-quiet 2> log.txt', shell=True, preexec_fn=os.setsid)
+            self.p_miner = subprocess.Popen('/usr/bin/minerd -a sha256d -o stratum+tcp://stratum.bitcoin.cz:3333 -O cloudminer.worker1:9868UyAN 2>log.txt', shell=True, preexec_fn=os.setsid)
+            self.p_monitor = Process(target = self.monitor_cpu)
+            #sleep(5)
+            self.p_monitor.start()
         else:
             if self.p_monitor <> None:
                 print "TERMINAT-ing the monitor"
